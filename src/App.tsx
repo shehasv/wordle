a97ef96b928d16f6a52272e48f429cb8718d54ce
shehasv <shehasktpm@gmail.com>
2025-03-20 20:16:10 +0530

    validate and game over check added

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div>
-      <Playground />
+      <Playground solution={solution} />
       {solution}
     </div>
   );
--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import './Playground.css';
 
-const Playground = () => {
+const Playground = ({ solution }) => {
   const [wordInputs, setwordInputs] = useState(
     Array(6).fill(Array(5).fill(''))
   );
   const [numberOfTries, setNumberOfTries] = useState(0);
 
-  const updateCell = (rowIndex, colIndex, newValue) => {
+  const updateCell = (rowIndex: number, colIndex: number, newValue: string) => {
     setwordInputs((prevWordInputs) => {
       const newWordInputs = prevWordInputs.map((row) => [...row]);
       newWordInputs[rowIndex][colIndex] = newValue;
@@ -22,10 +22,17 @@ const Playground = () => {
 
     if (isLetter || isEnter || isBackspace) {
       if (isLetter) {
-        const availedSpace = wordInputs[numberOfTries].findIndex(
+        const availedIndex = wordInputs[numberOfTries].findIndex(
           (letter: string) => !letter
         );
-        updateCell(numberOfTries, availedSpace, event.key.toUpperCase());
+        updateCell(numberOfTries, availedIndex, event.key.toUpperCase());
+      } else if (isBackspace) {
+        const removeIndex = wordInputs[numberOfTries].findLastIndex(
+          (letter: string) => letter
+        );
+        updateCell(numberOfTries, removeIndex, '');
+      } else {
+        checkGameOver();
       }
     }
 
@@ -33,17 +40,43 @@ const Playground = () => {
     event.stopPropagation();
   };
 
+  const checkGameOver = () => {
+    if (wordInputs[numberOfTries].every((item: string) => item)) {
+      validateWord();
+      setNumberOfTries((prev) => prev + 1);
+    } else {
+      // Alert user to complete the word
+    }
+  };
+
+  const validateWord = () => {
+    const solutionArray = solution.split('');
+    wordInputs[numberOfTries].forEach((letter: string, index: number) => {
+      const cell = document.getElementById('cell' + numberOfTries + index);
+      if (letter == solutionArray[index]) {
+        if (cell) cell.classList.add('correct');
+      } else if (solutionArray.includes(letter)) {
+        if (cell) cell.classList.add('close');
+      } else {
+        if (cell) cell.classList.add('incorrect');
+      }
+    });
+  };
+
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
     return () => document.removeEventListener('keydown', handleKeyDown);
-  }, [wordInputs]);
+  }, [wordInputs, numberOfTries]);
 
   return (
     <div>
-      {wordInputs.map((row: Array<string>) => (
+      {wordInputs.map((row: Array<string>, rowIndex: number) => (
         <div className="word-row">
-          {row.map((word: string) => (
-            <div className="word-column"> {word} </div>
+          {row.map((word: string, letterIndex: number) => (
+            <div className="word-letter" id={'cell' + rowIndex + letterIndex}>
+              {' '}
+              {word}{' '}
+            </div>
           ))}
         </div>
       ))}
