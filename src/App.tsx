688366d94e8b8e041e28ad151cc33cb2d6c741b7
shehasv <shehasktpm@gmail.com>
2025-03-21 22:41:43 +0530

    solution setting logic changed

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,28 @@ import Playground from './components/Playground/Playground';
 const WORDS_API_URL = 'https://api.frontendexpert.io/api/fe/wordle-words';
 
 function App() {
-  const [words, setWords] = useState();
+  const [wordsList, setWords] = useState([]);
   const [solution, setSolution] = useState('');
 
   const fetchWords = async () => {
     const data = await fetch(WORDS_API_URL);
     const wordsList = await data.json();
     setWords(wordsList);
-    setSolution(
-      wordsList[Math.floor(Math.random() * (wordsList.length - 0 + 1) + 0)]
-    );
   };
 
   useEffect(() => {
     fetchWords();
   }, []);
 
+  useEffect(() => {
+    setSolution(
+      wordsList[Math.floor(Math.random() * (wordsList.length - 0 + 1) + 0)]
+    );
+  },[wordsList])
+
   return (
     <div>
       <Playground solution={solution} />
-      {solution}
     </div>
   );
 }
